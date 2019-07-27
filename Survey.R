26ab26efaa91e62ce4ed09fa2a9fc3c3e5ec990a
Toka <tianshu1991@msn.com>
2019-07-27 19:38:23 +1000

    modification Survey.R

--- Survey.R
+++ Survey.R
@@ -16,7 +16,8 @@ cat("model{
 library(rjags)
 
 myinits <- list(
-  list(theta = .5, n = nmax/2)
+  list(theta = .5, n = nmax/2),
+  list(theta = .1, n = 1)
 )
 
 jags.survey <- jags.model(file = 'Survey.txt',
@@ -24,7 +25,7 @@ jags.survey <- jags.model(file = 'Survey.txt',
                                  "nmax"=nmax,
                                  "m"=m),
                      inits = myinits,
-                     n.chains = 1,
+                     n.chains = 2,
                      n.adapt = 100)
 
 parameters <- c("theta", "n")
@@ -33,12 +34,17 @@ samps <- coda.samples( jags.survey,
                        parameters, 
                        n.iter=5000,
                        n.burnin=1,
-                       n.thin=1)
+                       n.thin=100)
 
 library(bayesplot)
 
 par(mar=c(5,5,1,1)) #set figure margin
 
+#some fun ways to visualize parameter distribution
+#https://cran.r-project.org/web/packages/bayesplot/vignettes/plotting-mcmc-draws.html
+
+mcmc_trace(samps, pars = c("n", "theta"))
 mcmc_hist(samps) #plot parameters seperately
 mcmc_scatter(samps, pars = c("n", "theta"), 
              size = 1.5, alpha = 0.5) #joint distributions
+mcmc_hex(samps, pars = c("theta", "n")) #hexgon heatmap
